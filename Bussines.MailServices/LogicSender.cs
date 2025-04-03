e2ccd7ea8d056ad0b06e10adff91c1f53644ddb4
Gandoler <gl.krutoi@mail.ru>
2025-04-03 12:50:50 +0300

    убрал еще парочку baseurl

--- Bussines.MailServices/LogicSender.cs
+++ Bussines.MailServices/LogicSender.cs
@@ -9,20 +9,18 @@ namespace Bussines.MailServices;
 public class LogicSender : ILogicSenderCong
 {
     private readonly HttpClient _httpClient;
-    private readonly string _baseUrl;
     private readonly ILogger<LogicSender> _logger;
 
-    public LogicSender(HttpClient httpClient, IConfiguration configuration, ILogger<LogicSender> logger)
+    public LogicSender(HttpClient httpClient, ILogger<LogicSender> logger)
     {
         _httpClient = httpClient;
         _logger = logger;
-        _baseUrl = configuration["ProxyMicroservice:BaseUrl"];
     }
 
     public async Task<string?> GetCongrStrAsync(int wishId)
     {
         _logger.LogInformation($"Получение поздравления с ID {wishId}");
-        var response = await _httpClient.GetAsync($"{_baseUrl}/api/mailbot/GetCongrString/{wishId}");
+        var response = await _httpClient.GetAsync($"/api/mailbot/GetCongrString/{wishId}");
         if (!response.IsSuccessStatusCode)
         {
             _logger.LogWarning($"Не удалось получить поздравление. Код: {response.StatusCode}");
@@ -34,7 +32,7 @@ public class LogicSender : ILogicSenderCong
     public async Task<string> GetEmailAsync(Guid AppId)
     {
         _logger.LogInformation($"Получение email для AppId: {AppId}");
-        var response = await _httpClient.GetAsync($"{_baseUrl}/api/mailbot/email/{AppId}");
+        var response = await _httpClient.GetAsync($"/api/mailbot/email/{AppId}");
         if (!response.IsSuccessStatusCode)
         {
             _logger.LogWarning($"Email не найден. Код: {response.StatusCode}");
@@ -46,7 +44,7 @@ public class LogicSender : ILogicSenderCong
     public async Task<List<FriendDto>> GetTodayBirthdayAsync()
     {
         _logger.LogInformation("Получение списка именинников на сегодня");
-        var response = await _httpClient.GetAsync($"{_baseUrl}/api/mailbot/birthdays/today");
+        var response = await _httpClient.GetAsync($"/api/mailbot/birthdays/today");
         if (!response.IsSuccessStatusCode)
         {
             _logger.LogWarning($"Ошибка при получении списка. Код: {response.StatusCode}");
@@ -58,7 +56,7 @@ public class LogicSender : ILogicSenderCong
     public async Task<PozdrikIdDto?> GetWishIdAsync(FriendDto friendDto)
     {
         _logger.LogInformation($"Получение ID поздравления для {friendDto.FriendUsername}");
-        var response = await _httpClient.PostAsJsonAsync($"{_baseUrl}/api/mailbot/getPozdrikId", friendDto);
+        var response = await _httpClient.PostAsJsonAsync($"/api/mailbot/getPozdrikId", friendDto);
             
         
         if (!response.IsSuccessStatusCode)
